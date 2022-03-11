f6f4fcd00950e01cf7a1616f3cf7f7e10ce35590
Varun Bhatt <42655062+varunbhtt21@users.noreply.github.com>
2022-03-11 15:40:49 +0530

    Made some changes in problem11

--- Day4_ConditionalStatements/problem11.js
+++ Day4_ConditionalStatements/problem11.js
@@ -1,14 +1,14 @@
 // Discount Available
 // No Discount
 
-var total_bill = 450;
+var total_bill = 8000;
 var discount_start_price = 500;
 
 if(total_bill >= discount_start_price)
 {
-  console.log("Discount Available");
+  console.log("Discount Available for All");
 }
 else
 {
-  console.log("No Discount");
+  console.log("No Discount for all");
 }
